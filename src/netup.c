ccf7d7828b46b730686c27135d257f43dfbb9203
Lluis Campos <lluis.campos@northern.tech>
2024-09-18 21:17:34 +0200

    feat: Adapt reference application for WiFi on ESP32S3 DevKit
    * Modify `netup` to support wifi connection. After issuing the connect
      it just waits for IP address event. This is not very robust but will
      do for controlled demo scenarios.
    * Added KConfig to statically configure the SSID/PSK for the wifi
    * Added the new board to the README - this shit works \o/
    
    Kudos to Lars who actually figure out what we need to make it work!
    
    Changelog: None
    Ticket: MEN-7531
    
    Co-authored-by: Lars Erik Wik <lars.erik.wik@northern.tech>
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- src/netup.c
+++ src/netup.c
@@ -15,22 +15,60 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(mender_app, LOG_LEVEL_DBG);
 
-/* This piece of code is heavily inspired by Zephyr OS project sample:
+/* This piece of code is heavily inspired by Zephyr OS project samples:
  * https://github.com/zephyrproject-rtos/zephyr/tree/v3.7.0/samples/net/dhcpv4_client
+ * https://github.com/zephyrproject-rtos/zephyr/tree/v3.7.0/samples/net/cloud/tagoio_http_post
  *
  * The main entry point, netup_wait_for_network, will set up the callbacks for the network
- * management and wait for a the device to obtain a v4 IP address. The wait is controlled with
- * a simple semaphore. */
+ * management and wait for NET_EVENT_IPV4_ADDR_ADD event (iow, the device obtained an IP address.
+ * If WIFI configuration is enabled, a CONNECT request is issued and it is assumed that obtaining
+ * the IP address is managed somewhere else.
+ * The wait from netup_wait_for_network for the described event is controlled with a semaphore. */
 
 #include <assert.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_mgmt.h>
+#if defined(CONFIG_WIFI)
+#include <zephyr/net/wifi_mgmt.h>
+#endif
 
 K_SEM_DEFINE(network_ready_sem, 0, 1);
 
 static struct net_mgmt_event_callback mgmt_cb;
+
+#if defined(CONFIG_WIFI)
+
+static struct wifi_connect_req_params cnx_params = {
+    .ssid        = CONFIG_MENDER_APP_WIFI_SSID,
+    .ssid_length = strlen(CONFIG_MENDER_APP_WIFI_SSID),
+    .psk         = CONFIG_MENDER_APP_WIFI_PSK,
+    .psk_length  = strlen(CONFIG_MENDER_APP_WIFI_PSK),
+    .channel     = 0,
+    .security    = WIFI_SECURITY_TYPE_PSK,
+};
+
+static void
+wifi_connect(struct net_if *iface) {
+    int            ret   = 0;
+
+    LOG_INF("Connecting to wireless network %s...", cnx_params.ssid);
+
+    int nr_tries = 20;
+    while (nr_tries-- > 0) {
+        ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &cnx_params, sizeof(struct wifi_connect_req_params));
+        if (ret == 0) {
+            break;
+        }
+
+        /* Let's wait few seconds to allow wifi device be on-line */
+        LOG_ERR("Connect request failed %d. Waiting iface be up...\n", ret);
+        k_msleep(500);
+    }
+}
+
+#endif
 
 static void
 event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface) {
@@ -71,7 +109,13 @@ netup_wait_for_network() {
     struct net_if *iface = net_if_get_default();
     LOG_INF("Using net interface %s, index=%d", net_if_get_device(iface)->name, net_if_get_by_iface(iface));
 
+#if defined(CONFIG_WIFI)
+    wifi_connect(iface);
+#else
+    /* For WIFI, it is expected that the dhcp client is started somehow by the network management.
+    This is the case for example for ESP32-S3 with configuration option WIFI_STA_AUTO_DHCPV4 */
     net_dhcpv4_start(iface);
+#endif
 
     // Wait for network
     LOG_INF("Waiting for network up...");
