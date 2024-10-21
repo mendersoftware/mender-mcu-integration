772be2927d511fb282269ea0bfdb64fe414dcd38
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2024-10-21 15:25:38 +0200

    test: PoC integration test framework
    Integration test framework for testing mender-mcu client with
    native-sim.
    Still requires work - e.g. making the demo server work, and probably
    more work on the testing framework itself.
    It uses  `integration` as a submodulel, so you need to init it.
    
    You have to set tenant token, auth_token etc. manually, and
    I've only tested it with 'hosted.mender.io'.
    
    I went with the compile-in approach:
    You can modify the callbacks directly in the test. You do this by
    utilizing `set_callback()` from `helpers`. The available callbacks
    you can modify are stored in `callbacks.py`. The callbacks have an #ifdef,
    and the python script creates a header-file which is imported by the
    test-update-module.
    
    The test-update-module and the main-file for the tests are located in
    the `src/` in the integration-tests directory.
    
    Ticket: MEN-7599
    
    Signed-off-by: Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>

---
+++ tests/integration/definitions.py
@@ -0,0 +1,45 @@
+# Copyright 2024 Northern.tech AS
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+################
+# Callbacks
+################
+
+# Callbacks defined in the update module
+UM_DOWNLOAD_CALLBACK = "UM_DOWNLOAD_CALLBACK"
+UM_INSTALL_CALLBACK = "UM_INSTALL_CALLBACK"
+UM_REBOOT_CALLBACK = "UM_REBOOT_CALLBACK"
+UM_VERIFY_REBOOT_CALLBACK = "UM_VERIFY_REBOOT_CALLBACK"
+UM_COMMIT_CALLBACK = "UM_COMMIT_CALLBACK"
+UM_ROLLBACK_CALLBACK = "UM_ROLLBACK_CALLBACK"
+UM_ROLLBACK_REBOOT_CALLBACK = "UM_ROLLBACK_REBOOT_CALLBACK"
+UM_FAILURE_CALLBACK = "UM_FAILURE_CALLBACK"
+
+
+# Callback defined in main
+NETWORK_CONNECT_CALLBACK = "NETWORK_CONNECT_CALLBACK"
+NETWORK_RELEASE_CALLBACK = "NETWORK_RELEASE_CALLBACK"
+DEPLOYMENT_STATUS_CALLBACK = "DEPLOYMENT_STATUS_CALLBACK"
+RESTART_CALLBACK = "RESTART_CALLBACK"
+GET_IDENTITY_CALLBACK = "GET_IDENTITY_CALLBACK"
+
+
+################
+# Variables
+################
+
+# Update module requires reboot
+UM_REQUIRES_REBOOT = "UM_REQUIRES_REBOOT"
+# Update module supports rollback
+UM_SUPPORTS_ROLLBACK = "UM_SUPPORTS_ROLLBACK"
