d8814fd4df2b628586fabcd6b76c18189a2d4725
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2025-01-13 17:46:10 +0100

    fix: Reduce the size of the MbedTLS' heap buffer
    By reducing the size of the TLS output buffer we can also reduce
    the size of the MbedTLS' heap buffer (where MbedTLS allocates its
    internal buffers from).
    
    Ticket: MEN-7807
    Changelog: none
    Signed-off-by: Vratislav Podzimek <vratislav.podzimek@northern.tech>

---
+++ config-tls-mender.h
@@ -0,0 +1,25 @@
+// Copyright 2025 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+/* Zephyr's config-tls-generic.h defines this macro to the value of
+   CONFIG_MBEDTLS_SSL_MAX_CONTENT_LEN. However, that config value is used for
+   both input and output content and we can only use smaller output content
+   length because how much data is sent to us is not under our control (the
+   max_fragment_len TLS 1.2 extension seems to be ignored by the webservers we
+   communicate with). Prevent redefinition warnings by undefining the macro
+   first. */
+#ifdef MBEDTLS_SSL_OUT_CONTENT_LEN
+#undef MBEDTLS_SSL_OUT_CONTENT_LEN
+#endif
+#define MBEDTLS_SSL_OUT_CONTENT_LEN 2048
