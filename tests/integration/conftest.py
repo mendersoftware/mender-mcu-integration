2ee4d48ea59a0987916639a8e56e220a51d5b1d1
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2025-03-04 09:30:07 +0100

    chore: delete build directory
    Ensure that we delete the build directory after we've collected the
    coverage.
    
    Signed-off-by: Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -14,6 +14,7 @@
 
 import os
 import sys
+import shutil
 import logging
 import tempfile
 import subprocess
@@ -95,6 +96,7 @@ def get_coverage(request, get_build_dir):
         subprocess.check_call(command)
     except subprocess.CalledProcessError as err:
         pytest.fail(err.stderr)
+    shutil.rmtree(get_build_dir)
 
 
 @pytest.fixture(scope="session", autouse=True)
